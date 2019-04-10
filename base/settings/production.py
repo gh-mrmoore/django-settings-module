aa798c546c51f76f8ce679ee3fbfccff5aacc1f7
Matthew Moore <matt.rmoore@gmail.com>
2019-04-09 23:50:08 -0500

    configuring production settings for heroku deployment

--- base/settings/production.py
+++ base/settings/production.py
@@ -1,1 +1,15 @@
+import django_heroku
+
 TIME_ZONE = 'America/New_York'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    #from Heroku - for static file collection
+#extra locations to search for static files - from Heroku
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),   #changed from staticfiles to static to enable static files on 'base'
+)
+
+django_heroku.settings(locals())
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
